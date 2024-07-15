e8983d7ae7f8c551895663d6c04d2fcc87247e0a
nate <natericc@gmail.com>
2024-07-15 17:23:25 -0400

    index file start

---
+++ index.py
@@ -0,0 +1,19 @@
+num = int
+numString = string
+
+
+def main():
+    numberType = input "enter 1 to convert from decimal, 2 to convert from binary, 3 to convert from hex" :
+            if numberType = 1:
+                num = input "enter your number":
+                decimalNum(number)
+            elif numberType = 2:
+                num = input "enter your number":
+                binaryNum(number)
+            elif numberType =3:
+                numString = input "enter your number":
+                hexNum(number)
+            else
+                print ("please choose 1, 2, or 3")
+
+def decimalNum()
