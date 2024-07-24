bf8144f1915832d39b7b58fe1380738fda7361d0
nate <natericc@gmail.com>
2024-07-24 15:55:20 -0400

    decimal conversion

--- index.py
+++ index.py
@@ -1,19 +1,53 @@
-num = int
-numString = string
+num
+
 
 
 def main():
+    num = input "enter your number":
     numberType = input "enter 1 to convert from decimal, 2 to convert from binary, 3 to convert from hex" :
             if numberType = 1:
-                num = input "enter your number":
                 decimalNum(number)
             elif numberType = 2:
-                num = input "enter your number":
                 binaryNum(number)
             elif numberType =3:
-                numString = input "enter your number":
                 hexNum(number)
+            elif numberType = 4
+                end
             else
                 print ("please choose 1, 2, or 3")
 
-def decimalNum()
+def decimalNum():
+num = int(input("number"))  
+bin = num
+binaryList = []
+while bin != 0:
+    if bin % 2 == 0:
+        binaryList.append(0)
+    else:
+        binaryList.append(1)
+    bin = bin // 2   
+
+binaryList.reverse()
+print(binaryList)
+
+hex_num = num
+hexDigits = "0123456789ABCDEF"
+hexList = []
+
+while hex_num != 0:
+    remainder = hex_num % 16
+    hexList.append(hexDigits[remainder])
+    hex_num = hex_num // 16
+
+hexList.reverse()
+hex_string = ''.join(hexList)
+print(hexList)
+
+print(num + " in decimal is " + bin + " in binary " + " and " + hex_num " in hexadecimal")
+
+
+
+def binaryNum():
+
+
+def hexNum():
